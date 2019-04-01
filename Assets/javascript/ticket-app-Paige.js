d7056ae6f9a5ec26b394fd8f2d1de6099d7fac19
Paige Lieske <paigeproctor@comcast.net>
2019-04-01 16:40:10 -0400

    updated search, added modal

--- Assets/javascript/ticket-app-Paige.js
+++ Assets/javascript/ticket-app-Paige.js
@@ -1,5 +1,5 @@
-$("#submitbtn").on("click", function (evt) {
-    evt.preventDefault();
+function mainSearch() {
+
     $("#results").empty();
 
     var eventName = $("#eventName").val().trim();
@@ -10,14 +10,14 @@ $("#submitbtn").on("click", function (evt) {
     eventWhen = moment(eventWhen).format("MM-DD-YYYY");
 
     var eventWhere = $("#city").val().trim();
+    eventWhere = eventWhere.toLowerCase();
 
-    // eventWhere = eventWhere.toLowerCase();
     // eventName = eventName.replace(/\s+/g, "-")
-    console.log("what: " + eventName);
-    console.log("when: " + eventWhen);
-    console.log("where: " + eventWhere);
+    console.log("eventName: " + eventName);
+    console.log("eventWhen: " + eventWhen);
+    console.log("eventWhere: " + eventWhere);
 
-    var queryURL = "https://api.seatgeek.com/2/events/?client_id=MTU4NDczMDN8MTU1MzEyNzc0Ny42OA&client_secret=dda067dbe95284017c0a01a3ef629be429e1fb11f9a8317ad3a8ea50ac4f6e58&q=" + eventName + "&per_page=50";
+    var queryURL = "https://api.seatgeek.com/2/events/?client_id=MTU4NDczMDN8MTU1MzEyNzc0Ny42OA&client_secret=dda067dbe95284017c0a01a3ef629be429e1fb11f9a8317ad3a8ea50ac4f6e58&q=" + eventName + "&per_page=100";
 
     $.ajax({
         url: queryURL,
@@ -25,13 +25,15 @@ $("#submitbtn").on("click", function (evt) {
     }).then(function (response) {
         console.log(response);
         $('#results').html('<tbody></tbody>');
-        var results = response.events;
-        // $(".results").empty();
-        console.log(results.length);
-        console.log("blah");
 
-        if (!results.length) {
-            $("#results").text("Your search has 0 results.  Please try again.");
+        var results = response.events;
+        console.log(results.length);
+
+        if (!results.length || !eventName) {
+            $("#results").html("Your search has 0 results.  Please try again.");
+            $("#eventName").val("");
+            $("#date").val("");
+            $("#city").val("");
             return;
         }
 
@@ -44,6 +46,7 @@ $("#submitbtn").on("click", function (evt) {
             var where = results[i].venue.display_location;
 
             var whereValidation = results[i].venue.city;
+            whereValidation = whereValidation.toLowerCase();
 
             var when = results[i].datetime_local;
             when = moment(when).format("MM-DD-YYYY @ h:mm A");
@@ -51,67 +54,71 @@ $("#submitbtn").on("click", function (evt) {
             var whenValidation = moment(when).format("MM-DD-YYYY");
             console.log(whenValidation);
 
-            var ticketLink = $("<a>").text("Visit SeatGeek to View");
+            var ticketLink = $("<a>");
             ticketLink.attr("href", results[i].url);
-
-            // console.log(results.length);
-            // console.log("blah");
-
-            // if (eventMainTitle.indexOf(eventName) === -1) {
-            //     console.log("you failed");
-            // }
+            var ticketImage = $("<img src = seatgeek.png>");
+            ticketImage.addClass("seatgeek");
+            ticketLink.html(ticketImage);
 
             if (eventMainTitle.indexOf(eventName) > -1 && whereValidation.indexOf(eventWhere) > -1 && whenValidation.indexOf(eventWhen) > -1) {
-                // var table = $("<table>");
-                // var tableBody = $("<tbody>");
+                console.log("if statement: " + whenValidation);
                 var newRow = $("<tr>").append(
                     $("<td>").append(eventShortTitle),
                     $("<td>").append(where),
                     $("<td>").append(when),
                     $("<td>").append(ticketLink)
                 )
-                // $(table).append(tableBody);
                 $('#results tbody').append(newRow);
-                // $("#results").prepend(table);
-                clearInterval(window.animate);
-                $("#bdy").css("background", "#fff")
             }
-            else if (eventMainTitle.indexOf(eventName) > -1 && whereValidation.indexOf(eventWhere) > -1) {
-                // var table = $("<table>");
-                // var tableBody = $("<tbody>");
+
+            else if (whenValidation.indexOf(eventWhen) > -1 && !eventWhere) {
+                console.log("else when statement: " + whenValidation);
                 var newRow = $("<tr>").append(
                     $("<td>").append(eventShortTitle),
                     $("<td>").append(where),
                     $("<td>").append(when),
                     $("<td>").append(ticketLink)
                 )
-                // $(table).append(tableBody);
                 $('#results tbody').append(newRow);
-                clearInterval(window.animate);
-                $("#bdy").css("background", "#fff")
-            };
-            // else if (eventMainTitle.indexOf(eventName) > -1 &&whenValidation.indexOf(eventWhen) > -1) {
-            //     var table = $("<table>");
-            //     var tableBody = $("<tbody>");
-            //     var newRow = $("<tr>").append(
-            //         $("<td>").append(eventShortTitle),
-            //         $("<td>").append(where),
-            //         $("<td>").append(when),
-            //         $("<td>").append(ticketLink)
-            //     )
-            //     $(table).append(tableBody);
-            //     $(tableBody).append(newRow);
-            //     $("#results").prepend(table);
-            //     clearInterval(window.animate);
-            //     $("#bdy").css("background", "#fff")
-            // };
+            }
+
+            else if (whereValidation.indexOf(eventWhere) > -1 && eventWhen === "Invalid date") {
+                console.log("else where statement: " + whereValidation);
+                var newRow = $("<tr>").append(
+                    $("<td>").append(eventShortTitle),
+                    $("<td>").append(where),
+                    $("<td>").append(when),
+                    $("<td>").append(ticketLink)
+                )
+                $('#results tbody').append(newRow);
+            }
+
+            // else if (whereValidation.indexOf(eventWhere) < 0) {
+            //     $("#results").html("Your search has 0 results.  Please try again.");
+            // }
+
+            $("#eventName").val("");
+            $("#date").val("");
+            $("#city").val("");
         }
     })
+}
 
-    $("#eventName").val("");
-    $("#date").val("");
-    $("#city").val("");
+$('body').keypress(function (e) {
+    var code = e.keyCode || e.which;
+
+    if (code === 13) {
+        e.preventDefault();
+        mainSearch();
+    }
+})
+
+$("#submitbtn").on("click", function () {
+    mainSearch();
 });
+
+
+
 
 $(".localEvents").on("click", function (evt) {
     evt.preventDefault();
@@ -120,54 +127,80 @@ $(".localEvents").on("click", function (evt) {
     eventType = eventType[0].id;
     console.log(eventType);
 
-    $("#results").empty();
+    $(".modal").show();
 
-    var eventWhere = $("#city").val().trim();
-    console.log(eventWhere);
+    $("#zipClick").on("click", function () {
 
-    var musicURL = "https://api.seatgeek.com/2/events/?client_id=MTU4NDczMDN8MTU1MzEyNzc0Ny42OA&client_secret=dda067dbe95284017c0a01a3ef629be429e1fb11f9a8317ad3a8ea50ac4f6e58&taxonomies.name=" + eventType + "&geoip=" + eventWhere + "&range=15mi&per_page=25";
+        var zip = $("#zipInput").val().trim();
+        console.log(zip);
 
-    $.ajax({
-        url: musicURL,
-        method: "GET"
-    }).then(function (response) {
+        $("#results").empty();
 
-        console.log(response);
-        $('#results').html('<tbody></tbody>');
-        var results = response.events;
-    
-        for (var i = 0; i < results.length; i++) {
+        var musicURL = "https://api.seatgeek.com/2/events/?client_id=MTU4NDczMDN8MTU1MzEyNzc0Ny42OA&client_secret=dda067dbe95284017c0a01a3ef629be429e1fb11f9a8317ad3a8ea50ac4f6e58&taxonomies.name=" + eventType + "&geoip=" + zip + "&range=15mi&per_page=25";
 
-            var eventMainTitle = results[i].title;
-            eventMainTitle = eventMainTitle.toLowerCase();
+        $.ajax({
+            url: musicURL,
+            method: "GET"
+        }).then(function (response) {
 
-            var eventShortTitle = $("<p>").text(results[i].short_title);
+            console.log(response);
+            $('#results').html('<tbody></tbody>');
+            var results = response.events;
 
-            var where = results[i].venue.display_location;
-            
-            var when = results[i].datetime_local;
-            when = moment(when).format("MM-DD-YYYY @ h:mm A");
-            
-            
-            var whenFuture = moment().add(7, "days");
-            whenFuture = moment(whenFuture).format("MM-DD-YYYY");
-            console.log(whenFuture);
+            for (var i = 0; i < results.length; i++) {
 
-            var ticketLink = $("<a>").text("Visit SeatGeek to View");
-            ticketLink.attr("href", results[i].url);
-           
-            if (results.length > 0 && whenFuture > when) {
-                var newRow = $("<tr>").append(
-                    $("<td>").append(eventShortTitle),
-                    $("<td>").append(where),
-                    $("<td>").append(when),
-                    $("<td>").append(ticketLink)
-                )
-                
-                $('#results tbody').append(newRow);
-                clearInterval(window.animate);
-                $("#bdy").css("background", "#fff")
+                var eventMainTitle = results[i].title;
+                eventMainTitle = eventMainTitle.toLowerCase();
+
+                var eventShortTitle = $("<p>").text(results[i].short_title);
+
+                var where = results[i].venue.display_location;
+
+                var when = results[i].datetime_local;
+                when = moment(when).format("MM-DD-YYYY @ h:mm A");
+
+
+                var whenFuture = moment().add(7, "days");
+                whenFuture = moment(whenFuture).format("MM-DD-YYYY");
+                console.log(whenFuture);
+
+                var ticketLink = $("<a>");
+                ticketLink.attr("href", results[i].url);
+                var ticketImage = $("<img src = seatgeek.png>");
+                ticketImage.addClass("seatgeek");
+                ticketLink.html(ticketImage);
+
+
+                if (results.length > 0 && whenFuture > when) {
+                    var newRow = $("<tr>").append(
+                        $("<td>").append(eventShortTitle),
+                        $("<td>").append(where),
+                        $("<td>").append(when),
+                        $("<td>").append(ticketLink)
+                    )
+
+                    $('#results tbody').append(newRow);
+                    $(".modal").hide();
+                    $('html').scrollTop(0);
+                }
             }
-        }
+        })
     })
-}); 
+})
+
+$(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function () {
+    $("#back2Top").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
